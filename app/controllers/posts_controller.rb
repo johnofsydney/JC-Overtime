947e418278eded1845ce3c3e405f7f797e620db4
John Coote <john_coote@rocketmail.com>
2018-11-20 19:19:29 +1100

    tidy before revert. faulty

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
     def create
       @post = Post.new(post_params)
       @post.user_id = current_user.id
-      binding.pry
+      # binding.pry
       if @post.save
         redirect_to @post, notice: "Your post was created successfully"
       else
@@ -31,7 +31,7 @@ class PostsController < ApplicationController
     private
 
     def post_params
-      params.require(:post).permit(:date, :rationale)
+      params.require(:post).permit(:date, :rationale, :user_id)
     end
 
     def set_post
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,21 +2,25 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
-  describe "creation" do 
+  describe "creation" do
 
-    before do 
-      @post = Post.create(date: Date.today, rationale: "some reason")
-    end 
+    before do
+      u1 = User.create(email: "test@test.com", password: "chicken", password_confirmation: "chicken", first_name: "Jon", last_name: "Snow")
+      login_as(u1, :scope => :current_user)
 
-    it "can be created" do 
-      
+      @post = Post.create(date: Date.today, rationale: "some reason", user_id: current_user.id)
+      # @post.user_id = current_user.id
+    end
+
+    it "can be created" do
+
       expect(@post).to be_valid
       # expect(@user).to be_valid
-    end 
+    end
 
-    it "cannot be created without a date and rational" do 
-      @post.date = nil 
+    it "cannot be created without a date and rational" do
+      @post.date = nil
       expect(@post).to_not be_valid
-    end 
-  end 
+    end
+  end
 end
