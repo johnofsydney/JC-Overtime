22b108003a7ffafe39c8cc907fba98bf6485ca63
John Coote <john_coote@rocketmail.com>
2018-11-20 20:25:23 +1100

    user login and associate with posts AOK

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,10 +2,17 @@ require 'rails_helper'
 
 describe 'navigate' do
 
+  before do
+    user = User.create(email: "test100@test.com", password: "chicken", password_confirmation: "chicken", first_name: "Jon", last_name: "Snow")
+    login_as(user, :scope => :user)
+    visit new_post_path
+  end
+
+
   describe 'index' do
     it "can be reached successfully" do
-        visit posts_path
-        expect(page.status_code).to eq(200)
+      visit posts_path
+      expect(page.status_code).to eq(200)
     end
 
     it "has a title of Posts" do
